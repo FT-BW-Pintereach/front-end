d3bb235a185f1bf508f7ed3b8c5374b0b81ee4f5
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-03-03 16:00:32 -0500

    working on modal and board
    Co-authored-by: vanesa-tamu <60890822+vanesa-tamu@users.noreply.github.com>

--- pintereach/src/components/BoardForm.js
+++ pintereach/src/components/BoardForm.js
@@ -1,25 +1,55 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
+function BoardForm() {
+	const initialState = {
+		category: ""
+	};
 
-function BoardForm(){
+	const [data, setData] = useState(initialState);
 
+	const handleInputChange = event => {
+		setData({
+			...data,
+			[event.target.name]: event.target.value
+		});
+	};
 
-    return(
-        <div>
-            <form>
-                <label htmlFor='category'>Select Category</label>
-                <input
-                    id='category'
-                    placeholder='Finance News'
-                    name='category'
-                    type='text'
-                />
-                <button>Submit</button>
-            </form>
-        </div>
-    )
+	const handleFormSubmit = event => {
+		event.preventDefault();
+		// axiosWithAuth()
+		// 	.post("", {
+		// 		category: data.category
+		// 	})
+		// 	.then(res => {
+		// 		setData(res);
+			
+		// 		setData(initialState);
+		// 	})
+		// 	.catch(err => {
+		// 		console.log("error posting data", err);
+		// 	});
+	};
+
+	return (
+		<div>
+			<form onSubmit={handleFormSubmit}>
+				<label htmlFor="category">Create a category</label>
+				<input
+					id="category"
+					placeholder="Finance News"
+					name="category"
+					type="text"
+					value={data.name}
+					onChange={handleInputChange}
+					required
+				/>
+				<button>Submit</button>
+			</form>
+		</div>
+	);
 }
 
-export default BoardForm
+export default BoardForm;
 
 // manage state with reducers between articles and this
